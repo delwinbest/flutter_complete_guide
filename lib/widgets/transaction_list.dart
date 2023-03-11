b535528969df30c486326d4e9e23e3c51ff0f486
Delwin Best <mail@delwinbest.com>
2023-03-11 13:26:49 -0800

    Sizing components in landscape mode with layoutbuilder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,54 +5,61 @@ import 'package:intl/intl.dart';
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function deleteHandler;
-  const TransactionList({required this.transactions, required this.deleteHandler, super.key});
+  const TransactionList(
+      {required this.transactions, required this.deleteHandler, super.key});
 
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-      ? Column(
-          children: <Widget>[
-            Text(
-              "No transactions added yet!",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
-                )),
-          ],
-        )
-      : ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-              margin:
-                  const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-              elevation: 5,
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 30,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: FittedBox(
-                        child: Text('\$${transactions[index].amount}')),
-                  ),
-                ),
-                title: Text(
-                  transactions[index].title,
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  "No transactions added yet!",
                   style: Theme.of(context).textTheme.titleMedium,
                 ),
-                subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date)),
-                trailing: IconButton(icon: const Icon(Icons.delete), color: Theme.of(context).colorScheme.error , onPressed: () {deleteHandler(transactions[index].id);} ),
+                const SizedBox(
+                  height: 20,
                 ),
+                SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
             );
-          },
-          itemCount: transactions.length,
-        );
+          })
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                elevation: 5,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: FittedBox(
+                          child: Text('\$${transactions[index].amount}')),
+                    ),
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Theme.of(context).colorScheme.error,
+                      onPressed: () {
+                        deleteHandler(transactions[index].id);
+                      }),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
