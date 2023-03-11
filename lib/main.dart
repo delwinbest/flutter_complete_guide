7fe1058bd4d57d1400ad264d1a305a9183fd8894
Delwin Best <mail@delwinbest.com>
2023-03-11 12:48:56 -0800

    User Layout builder and MediaInfo to get viewport sizes and max heights dynamic using %

--- lib/main.dart
+++ lib/main.dart
@@ -50,17 +50,35 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final List<Transaction> _userTransactions = [
     Transaction(
-        id: const Uuid().v4(), title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+        id: const Uuid().v4(),
+        title: 'New Shoes',
+        amount: 69.99,
+        date: DateTime.now()),
     Transaction(
-        id: const Uuid().v4(), title: 'Groceries', amount: 16.99, date: DateTime.now()),
+        id: const Uuid().v4(),
+        title: 'Groceries',
+        amount: 16.99,
+        date: DateTime.now()),
     Transaction(
-        id: const Uuid().v4(), title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+        id: const Uuid().v4(),
+        title: 'New Shoes',
+        amount: 69.99,
+        date: DateTime.now()),
     Transaction(
-        id: const Uuid().v4(), title: 'Groceries', amount: 16.99, date: DateTime.now()),
+        id: const Uuid().v4(),
+        title: 'Groceries',
+        amount: 16.99,
+        date: DateTime.now()),
     Transaction(
-        id: const Uuid().v4(), title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+        id: const Uuid().v4(),
+        title: 'New Shoes',
+        amount: 69.99,
+        date: DateTime.now()),
     Transaction(
-        id: const Uuid().v4(), title: 'Groceries', amount: 16.99, date: DateTime.now())
+        id: const Uuid().v4(),
+        title: 'Groceries',
+        amount: 16.99,
+        date: DateTime.now())
   ];
 
   List<Transaction> get _recentTransactions {
@@ -73,7 +91,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTransaction(String newtitle, double newAmount, DateTime chosenDate) {
+  void _addNewTransaction(
+      String newtitle, double newAmount, DateTime chosenDate) {
     final newTransaction = Transaction(
         id: DateTime.now().toString(),
         title: newtitle,
@@ -99,39 +118,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _deleteTransaction(String transactionId) {
     setState(() {
-      _userTransactions.removeWhere((transaction) => transaction.id == transactionId);
+      _userTransactions
+          .removeWhere((transaction) => transaction.id == transactionId);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Personal Expenses',
-          style: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 20,
-              fontWeight: FontWeight.bold),
-        ),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () => _startAddNewTransaction(context),
-              icon: const Icon(Icons.add))
-        ],
+    final appBar = AppBar(
+      title: const Text(
+        'Personal Expenses',
+        style: TextStyle(
+            fontFamily: 'OpenSans', fontSize: 20, fontWeight: FontWeight.bold),
       ),
+      actions: <Widget>[
+        IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: const Icon(Icons.add))
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(
-              recentTransactions: _recentTransactions,
-            ),
-            TransactionList(
-              transactions: _userTransactions,
-              deleteHandler: _deleteTransaction
+            SizedBox(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.3,
+              child: Chart(
+                recentTransactions: _recentTransactions,
               ),
+            ),
+            SizedBox(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.7,
+              child: TransactionList(
+                  transactions: _userTransactions,
+                  deleteHandler: _deleteTransaction),
+            ),
           ],
         ),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,52 +9,50 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 600,
-        child: transactions.isEmpty
-            ? Column(
-                children: <Widget>[
-                  Text(
-                    "No transactions added yet!",
-                    style: Theme.of(context).textTheme.titleMedium,
+    return transactions.isEmpty
+      ? Column(
+          children: <Widget>[
+            Text(
+              "No transactions added yet!",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
+                )),
+          ],
+        )
+      : ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+              elevation: 5,
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: FittedBox(
+                        child: Text('\$${transactions[index].amount}')),
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                      height: 200,
-                      child: Image.asset(
-                        'assets/images/waiting.png',
-                        fit: BoxFit.cover,
-                      )),
-                ],
-              )
-            : ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: FittedBox(
-                              child: Text('\$${transactions[index].amount}')),
-                        ),
-                      ),
-                      title: Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      subtitle: Text(
-                          DateFormat.yMMMd().format(transactions[index].date)),
-                      trailing: IconButton(icon: const Icon(Icons.delete), color: Theme.of(context).colorScheme.error , onPressed: () {deleteHandler(transactions[index].id);} ),
-                      ),
-                  );
-                },
-                itemCount: transactions.length,
-              ));
+                ),
+                title: Text(
+                  transactions[index].title,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date)),
+                trailing: IconButton(icon: const Icon(Icons.delete), color: Theme.of(context).colorScheme.error , onPressed: () {deleteHandler(transactions[index].id);} ),
+                ),
+            );
+          },
+          itemCount: transactions.length,
+        );
   }
 }
