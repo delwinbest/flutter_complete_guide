0e68f422379cf131d41c0af9b617f647243b831e
Delwin Best <mail@delwinbest.com>
2023-03-21 19:13:01 -0700

    Initial GridView and scafold

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES
+            .map((categoryData) => CategoryItem(
+                title: categoryData.title, color: categoryData.color))
+            .toList(),
+      ),
     );
   }
 }
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -15,7 +15,8 @@ class CategoryItem extends StatelessWidget {
           gradient: LinearGradient(
               colors: [color.withOpacity(0.7), color],
               begin: Alignment.topLeft,
-              end: Alignment.bottomRight)),
+              end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15)),
     );
   }
 }
