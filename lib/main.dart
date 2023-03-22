30c6f9d7bd8efe8243a8884c3d8ec26da2a74346
Delwin Best <mail@delwinbest.com>
2023-03-21 19:31:17 -0700

    removed deprecated attribs and applied const

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
           fontFamily: 'Raleway',
           textTheme: ThemeData.light().textTheme.copyWith(
-              bodyLarge: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              bodyMedium: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyLarge: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyMedium: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               titleLarge:
-                  TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
-              titleMedium: TextStyle(
+                  const TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
+              titleMedium: const TextStyle(
                   fontSize: 20,
                   fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold))),
+                  fontWeight: FontWeight.bold)),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+              .copyWith(secondary: Colors.amber)),
       home: const CategoriesScreen(),
     );
   }
