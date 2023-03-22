57b462b97b27b048f50caa855cbb30e5df5c2265
Delwin Best <mail@delwinbest.com>
2023-03-21 19:29:38 -0700

    Added Theming and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyMedium: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleLarge:
+                  TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
+              titleMedium: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
       home: const CategoriesScreen(),
     );
   }
