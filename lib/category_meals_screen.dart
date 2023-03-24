e53f387ef228e3662226ba21ddbdf562bab09c27
Delwin Best <mail@delwinbest.com>
2023-03-23 20:48:38 -0700

    ENUMS, listbuilder

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // const CategoryMealsScreen(
@@ -7,19 +8,26 @@ class CategoryMealsScreen extends StatelessWidget {
   // final String categoryTitle;
   static const routeName = '/category-meals';
 
+  const CategoryMealsScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'] as String;
-    // final categoryId = routeArgs['id'];
+    final categoryId = routeArgs['id'];
+    final List categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: const Center(
-        child: Text('Recipes for this category'),
-      ),
+      body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length),
     );
   }
 }
