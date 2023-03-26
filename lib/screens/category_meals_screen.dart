d8d9cc5991919f708c95ab17a353e234fac28f5b
Delwin Best <mail@delwinbest.com>
2023-03-26 14:11:40 -0700

    add mealItem widget with image from network, clip and fit

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './dummy_data.dart';
+import 'package:flutter_complete_guide/widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // const CategoryMealsScreen(
@@ -25,7 +26,12 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                affordability: categoryMeals[index].affordability,
+                complexity: categoryMeals[index].complexity);
           },
           itemCount: categoryMeals.length),
     );
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem(
+      {super.key,
+      required this.title,
+      required this.imageUrl,
+      required this.duration,
+      required this.affordability,
+      required this.complexity});
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  void selectMeal() => {};
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
