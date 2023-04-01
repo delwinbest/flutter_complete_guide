5afcf0ec82e18579f72df11a7d00e0b2825c83de
Delwin Best <mail@delwinbest.com>
2023-04-01 14:18:48 -0700

    Meal cards with enum text getters

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -16,6 +16,32 @@ class MealItem extends StatelessWidget {
   final Affordability affordability;
   void selectMeal() => {};
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.simple:
+        return 'Simple';
+      case Complexity.challenging:
+        return 'Challenging';
+      case Complexity.difficult:
+        return 'Difficult';
+      default:
+        return 'None';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.affordable:
+        return 'Affordable';
+      case Affordability.luxurious:
+        return 'Luxurious';
+      case Affordability.pricey:
+        return 'Pricey';
+      default:
+        return 'None';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -38,8 +64,55 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: const TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min')
+                    ],
+                  ),
+                  Row(children: [
+                    const Icon(Icons.work),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Text(complexityText)
+                  ]),
+                  Row(children: [
+                    const Icon(Icons.attach_money),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Text(affordabilityText)
+                  ])
+                ],
+              ),
             )
           ],
         ),
