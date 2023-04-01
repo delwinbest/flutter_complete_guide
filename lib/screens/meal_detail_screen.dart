2f49796af0216271f9a5a96426188fbe0010254d
Delwin Best <mail@delwinbest.com>
2023-04-01 15:05:58 -0700

    image on meal detail page

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({super.key});
@@ -10,10 +11,20 @@ class MealDetailScreen extends StatelessWidget {
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments! as Map<String, String>;
     final mealId = routeArgs['id'] as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(title: Text(mealId)),
-      body: Center(
-        child: Text('The Meal = $mealId'),
+      appBar: AppBar(title: Text(selectedMeal.title)),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
       ),
     );
   }
