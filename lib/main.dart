f4365d597c119e0a8f04452a5a56fe7a7bb93f98
Delwin Best <mail@delwinbest.com>
2023-04-01 14:49:21 -0700

    new Detail screen with scaffold and route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/meal_detail_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/category_meals_screen.dart';
 
@@ -32,7 +33,8 @@ class MyApp extends StatelessWidget {
       initialRoute: CategoriesScreen.routeName,
       routes: {
         CategoriesScreen.routeName: (ctx) => const CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()
+        CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => const MealDetailScreen()
       },
     );
   }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/models/meal.dart';
+import 'package:flutter_complete_guide/screens/meal_detail_screen.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem(
       {super.key,
+      required this.id,
       required this.title,
       required this.imageUrl,
       required this.duration,
@@ -14,7 +16,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
-  void selectMeal() => {};
+  final String id;
 
   String get complexityText {
     switch (complexity) {
@@ -25,7 +27,7 @@ class MealItem extends StatelessWidget {
       case Complexity.difficult:
         return 'Difficult';
       default:
-        return 'None';
+        return 'Unknown';
     }
   }
 
@@ -38,14 +40,19 @@ class MealItem extends StatelessWidget {
       case Affordability.pricey:
         return 'Pricey';
       default:
-        return 'None';
+        return 'Unknown';
     }
+  }
+
+  void selectMeal(BuildContext context) {
+    Navigator.of(context)
+        .pushNamed(MealDetailScreen.routeName, arguments: {'id': id});
   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
         elevation: 4,
