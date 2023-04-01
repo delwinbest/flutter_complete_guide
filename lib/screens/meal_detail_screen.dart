896165b5f62c929c3acf206d7c0c7f351757c30a
Delwin Best <mail@delwinbest.com>
2023-04-01 15:17:37 -0700

    add scrollable container

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -16,12 +16,39 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(title: Text(selectedMeal.title)),
       body: Column(
         children: [
-          Container(
+          SizedBox(
             height: 300,
             width: double.infinity,
             child: Image.network(
               selectedMeal.imageUrl,
               fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Text('Ingredients',
+                style: Theme.of(context).textTheme.titleMedium),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            height: 200,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                    color: Theme.of(context).colorScheme.secondary,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: Text(selectedMeal.ingredients[index]),
+                    ));
+              },
+              itemCount: selectedMeal.ingredients.length,
             ),
           )
         ],
