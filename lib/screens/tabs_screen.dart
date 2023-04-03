5a4bf82da749169902dd7b06ce5eacddd83db6fb
Delwin Best <mail@delwinbest.com>
2023-04-03 14:20:17 -0700

    bottom tabs/navigation bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -12,6 +12,19 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  final List<Widget> _pages = [
+    const CategoriesScreen(),
+    const FavouritesScreen()
+  ];
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -19,19 +32,33 @@ class _TabsScreenState extends State<TabsScreen> {
         child: Scaffold(
           appBar: AppBar(
             title: const Text('Meals'),
-            bottom: const TabBar(tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favourites',
-              )
-            ]),
+            // bottom: const TabBar(tabs: <Widget>[
+            //   Tab(
+            //     icon: Icon(Icons.category),
+            //     text: 'Categories',
+            //   ),
+            //   Tab(
+            //     icon: Icon(Icons.star),
+            //     text: 'Favourites',
+            //   )
+            // ]),
           ),
-          body: TabBarView(
-              children: <Widget>[CategoriesScreen(), FavouritesScreen()]),
+          // body: TabBarView(
+          //     children: <Widget>[CategoriesScreen(), FavouritesScreen()]),
+          body: _pages[_selectedPageIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: _selectPage,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            selectedItemColor: Theme.of(context).colorScheme.secondary,
+            currentIndex: _selectedPageIndex,
+            unselectedItemColor: Colors.white,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.category), label: 'Categories'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.star), label: 'Favourites')
+            ],
+          ),
         ));
   }
 }
