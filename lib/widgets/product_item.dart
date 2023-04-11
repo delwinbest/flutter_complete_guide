9b72388c816241935fb26fa940749cdb31c1f91b
Delwin Best <mail@delwinbest.com>
2023-04-10 19:52:36 -0700

    Gridtilebar with leading and trailing buttons

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,6 +12,26 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imageUrl));
+    return GridTile(
+      footer: GridTileBar(
+        leading: IconButton(
+          icon: const Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.black54,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
