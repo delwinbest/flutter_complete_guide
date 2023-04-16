49a5d57b12cd622b7e6985c7f290d74504c31300
Delwin Best <mail@delwinbest.com>
2023-04-16 14:01:16 -0700

    when creating // instantiating an object, and NOT reuse, you should use create to instantiate a provider for the Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: ProductsProvider(),
+    return ChangeNotifierProvider(
+      create: (_) => ProductsProvider(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
