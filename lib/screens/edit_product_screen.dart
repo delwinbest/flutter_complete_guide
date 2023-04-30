128bb387ea361a01511a30fdd589cfa9a68ea7e9
Delwin Best <mail@delwinbest.com>
2023-04-30 12:45:25 -0700

    Multiline description field w. focusnode

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,6 +10,15 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +45,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
               keyboardType: TextInputType.number,
               focusNode: _priceFocusNode,
               textInputAction: TextInputAction.next,
-            )
+              onFieldSubmitted: (_) =>
+                  FocusScope.of(context).requestFocus(_priceFocusNode),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Description',
+              ),
+              keyboardType: TextInputType.multiline,
+              maxLines: 3,
+              focusNode: _descriptionFocusNode,
+            ),
           ],
         )),
       ),
