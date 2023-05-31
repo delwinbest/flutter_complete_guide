1aaaf6f9b6da380b169e37b779bbbcdf6d0f5544
Delwin Best <mail@delwinbest.com>
2023-05-31 20:34:50 +0200

    hero image fade in/out

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -59,16 +59,19 @@ class ProductItem extends StatelessWidget {
                 Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                     arguments: product.id);
               },
-              child: FadeInImage(
-                placeholder:
-                    const AssetImage('assets/images/product-placeholder.png'),
-                image: NetworkImage(product.imageUrl),
-                fit: BoxFit.cover,
-                //   Image.network(
-                //     product.imageUrl,
-                //     fit: BoxFit.cover,
-                //   ),
-                // )
+              child: Hero(
+                tag: product.id,
+                child: FadeInImage(
+                  placeholder:
+                      const AssetImage('assets/images/product-placeholder.png'),
+                  image: NetworkImage(product.imageUrl),
+                  fit: BoxFit.cover,
+                  //   Image.network(
+                  //     product.imageUrl,
+                  //     fit: BoxFit.cover,
+                  //   ),
+                  // )
+                ),
               )),
         ),
       ),
