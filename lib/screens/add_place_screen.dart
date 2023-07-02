098fdc18c11352e5d880d9da501bd4e23c3c9806
Delwin Best <mail@delwinbest.com>
2023-07-02 08:08:50 -0700

    create ImageAdd wdigets

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   const AddPlaceScreen({super.key});
@@ -9,25 +10,40 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add a New Place')),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text('User Input ...'),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  backgroundColor: Theme.of(context).colorScheme.secondary),
-              onPressed: () {},
-              icon: const Icon(Icons.add),
-              label: const Text('Add Place'),
-            )
-          ]),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        Expanded(
+            child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(labelText: 'Title'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ImageInput()
+              ],
+            ),
+          ),
+        )),
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              backgroundColor: Theme.of(context).colorScheme.secondary),
+          onPressed: () {},
+          icon: const Icon(Icons.add),
+          label: const Text('Add Place'),
+        )
+      ]),
     );
   }
 }
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  final File? _storedImage = null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 150,
+          height: 100,
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.grey, width: 1)),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : const Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+            child: TextButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.camera),
+          label: const Text('Take Picture'),
+        ))
+      ],
+    );
+  }
+}
