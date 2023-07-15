3ab34469fb3ce10e02fc951dfffe1cfcac2d558b
Delwin Best <mail@delwinbest.com>
2023-07-15 08:48:50 -0700

    subscribe and write to a stream

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -10,25 +10,37 @@ class ChatScreen extends StatelessWidget {
     return FutureBuilder(
         future: Firebase.initializeApp(),
         builder: (context, snapshot) {
-          return Scaffold(
-            body: ListView.builder(
-              itemBuilder: (ctx, index) => Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('This works'),
-              ),
-              itemCount: 10,
-            ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  FirebaseFirestore.instance
-                      .collection('chats/9Xoe4YCSdtTArb9qs60z/messages')
-                      .snapshots()
-                      .listen((data) {
-                    print(data);
-                  });
-                },
-                child: const Icon(Icons.add)),
-          );
+          return snapshot.connectionState == ConnectionState.waiting
+              ? const Center(
+                  child: Text('Connecting to database...'),
+                )
+              : Scaffold(
+                  body: StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('chats/9Xoe4YCSdtTArb9qs60z/messages')
+                          .snapshots(),
+                      builder: (ctx, streamSnapshot) {
+                        if (streamSnapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                        final documents = streamSnapshot.data!.docs;
+                        return ListView.builder(
+                          itemBuilder: (ctx, index) => Container(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(documents[index]['text'] as String),
+                          ),
+                          itemCount: streamSnapshot.data?.docs.length,
+                        );
+                      }),
+                  floatingActionButton: FloatingActionButton(
+                      onPressed: () {
+                        FirebaseFirestore.instance
+                            .collection('chats/9Xoe4YCSdtTArb9qs60z/messages')
+                            .add({'text': 'hehehe'});
+                      },
+                      child: const Icon(Icons.add)));
         });
   }
 }
