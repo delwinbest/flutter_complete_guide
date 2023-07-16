11d1e2fa0c47fbd5183e401fab3858b65c7ec64b
Delwin Best <mail@delwinbest.com>
2023-07-16 16:41:39 -0700

    add validation and signup/login logic

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -8,6 +8,21 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLogin = true;
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+    if (isValid) {
+      _formKey.currentState?.save();
+      print(_userEmail);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,27 +32,68 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: const ValueKey('email'),
                     keyboardType: TextInputType.emailAddress,
                     decoration:
                         const InputDecoration(labelText: 'Email address'),
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userEmail = newValue as String;
+                    },
                   ),
+                  if (_isLogin)
+                    TextFormField(
+                      key: const ValueKey('username'),
+                      decoration: const InputDecoration(labelText: 'Username'),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 4) {
+                          return 'Please enter a username longer than 4 characters.';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _userName = newValue as String;
+                      },
+                    ),
                   TextFormField(
-                    decoration: const InputDecoration(labelText: 'Username'),
-                  ),
-                  TextFormField(
+                    key: const ValueKey('password'),
                     decoration: const InputDecoration(labelText: 'Password'),
                     obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Please enter a password longer than 7 characters.';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userPassword = newValue as String;
+                    },
                   ),
                   const SizedBox(
                     height: 12,
                   ),
-                  ElevatedButton(onPressed: () {}, child: const Text('Login')),
+                  ElevatedButton(
+                      onPressed: _trySubmit,
+                      child: Text(_isLogin ? 'Login' : 'Signup')),
                   TextButton(
-                      onPressed: () {}, child: const Text('Create new account'))
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                      child: Text(_isLogin
+                          ? 'Create new account'
+                          : 'I already have an account'))
                 ],
               ),
             ),
