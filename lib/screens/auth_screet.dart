3a4f324a84c775051f6fa34f207e2db6c72eec26
Delwin Best <mail@delwinbest.com>
2023-07-24 19:03:14 -0700

    cleanup

--- lib/screens/auth_screet.dart
+++ lib/screens/auth_screet.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_complete_guide/widgets/auth/auth_form.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -48,7 +47,6 @@ class _AuthScreenState extends State<AuthScreen> {
       setState(() {
         _isLoading = false;
       });
-      print(error);
     }
   }
 
