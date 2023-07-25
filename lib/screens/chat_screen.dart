1272eecc683dc9592fffab7d4369a1fbf8645851
Delwin Best <mail@delwinbest.com>
2023-07-24 21:20:08 -0700

    New message handler with stream ordering using timeStamp

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/widgets/chat/messages.dart';
+import 'package:flutter_complete_guide/widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -9,45 +10,39 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Chat'),
-          actions: [
-            DropdownButton(
-              icon: Icon(
-                Icons.more_vert,
-                color: Theme.of(context).primaryIconTheme.color,
-              ),
-              items: const [
-                DropdownMenuItem(
-                  value: 'logout',
-                  child: Row(
-                    children: [
-                      Icon(Icons.exit_to_app),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Text('Logout')
-                    ],
-                  ),
-                )
-              ],
-              onChanged: (itemIdentifier) {
-                if (itemIdentifier == 'logout') {
-                  FirebaseAuth.instance.signOut();
-                }
-              },
-            )
-          ],
-        ),
-        body: const Column(
-          children: [Expanded(child: Messages())],
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              FirebaseFirestore.instance
-                  .collection('chats/9Xoe4YCSdtTArb9qs60z/messages')
-                  .add({'text': 'hehehe'});
+      appBar: AppBar(
+        title: const Text('Flutter Chat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: const [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Row(
+                  children: [
+                    Icon(Icons.exit_to_app),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text('Logout')
+                  ],
+                ),
+              )
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
             },
-            child: const Icon(Icons.add)));
+          )
+        ],
+      ),
+      body: const Column(
+        children: [Expanded(child: Messages()), NewMessage()],
+      ),
+    );
   }
 }
---
+++ lib/widgets/chat/new_message.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _textController = TextEditingController();
+  String _enteredMessage = "";
+
+  void _sendMessage() {
+    FocusScope.of(context).unfocus();
+    FirebaseFirestore.instance
+        .collection('chat')
+        .add({'text': _enteredMessage, 'createdAt': Timestamp.now()});
+    setState(() {
+      _enteredMessage = "";
+    });
+    _textController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+              child: TextField(
+            controller: _textController,
+            decoration: const InputDecoration(labelText: 'Send a message'),
+            onChanged: (value) {
+              setState(() {
+                _enteredMessage = value;
+              });
+            },
+          )),
+          IconButton(
+            onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
+            icon: const Icon(Icons.send),
+            color: Theme.of(context).primaryColor,
+          )
+        ],
+      ),
+    );
+  }
+}
