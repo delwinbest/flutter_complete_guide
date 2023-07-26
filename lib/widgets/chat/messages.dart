3f440385a813eb6438507f37c48c749b4f444533
Delwin Best <mail@delwinbest.com>
2023-07-25 19:40:53 -0700

    create MessageBubble with styling based on user.

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/chat/message_bubble.dart';
 
 class Messages extends StatelessWidget {
   const Messages({super.key});
@@ -21,7 +23,11 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs.length,
-          itemBuilder: (context, index) => Text(chatDocs[index]['text']),
+          itemBuilder: (context, index) => MessageBubble(
+              chatDocs[index]['text'],
+              chatDocs[index]['userId'] ==
+                  FirebaseAuth.instance.currentUser!.uid,
+              key: ValueKey(chatDocs[index].id)),
         );
       },
     );
