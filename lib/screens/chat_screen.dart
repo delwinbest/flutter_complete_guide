40ebcd7d8b5db86495db87c49eba579fcc826d3e
Delwin Best <mail@delwinbest.com>
2023-10-29 18:46:54 -0700

    add firebasecloud messaging to app and request permissions.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/widgets/chat/messages.dart';
 import 'package:flutter_complete_guide/widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    FirebaseMessaging.instance.requestPermission();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
